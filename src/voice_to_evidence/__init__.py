893fbdaa9607478d9f2b5d1f4ea50d7df9786648
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-05-08 13:50:24 +0300

    export intake question protocol

--- src/voice_to_evidence/__init__.py
+++ src/voice_to_evidence/__init__.py
@@ -10,16 +10,26 @@ from voice_to_evidence.intake import (
     load_intake,
     load_intake_from_file,
 )
+from voice_to_evidence.protocol import (
+    INTAKE_QUESTIONS,
+    IntakeQuestion,
+    iter_required_questions,
+    render_intake_protocol,
+)
 from voice_to_evidence.snapshot import render_snapshot
 
 __all__ = [
+    "INTAKE_QUESTIONS",
+    "IntakeQuestion",
     "IntakeRecord",
     "IntakeValidationError",
     "extract_intake_from_transcript",
+    "iter_required_questions",
     "load_intake",
     "load_intake_from_file",
     "parse_transcript_sections",
+    "render_intake_protocol",
     "render_snapshot",
 ]
 
-__version__ = "0.2.0"
+__version__ = "0.4.0"
